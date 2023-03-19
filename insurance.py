e08a69b2d06f5b04b77a66233fbd3b7b7172b22f
Dhanushkumar R <93417785+Idk507@users.noreply.github.com>
2023-03-19 11:43:23 +0530

    Update insurance.py

--- insurance.py
+++ insurance.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
+
 from sklearn.ensemble import RandomForestRegressor
 
 nav = st.sidebar.radio("Navigation",["About","Predict"])
